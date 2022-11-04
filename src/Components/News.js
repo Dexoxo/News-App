1bf3baa8bd511fe28829d0e0fd98d2ec9d869cd2
Dev Verma <dex.xerma@gmail.com>
2022-11-04 07:26:57 +0530

    initial commit

--- src/Components/News.js
+++ src/Components/News.js
@@ -53,7 +53,7 @@ export default class News extends Component {
     // handlePrevClick = async () => {
     //     this.setState({ page: this.state.page - 1 });
     //     this.updateNews();
-    // }
+    // } 
 
     // handleNextClick = async () => {
     //     this.setState({ page: this.state.page + 1 });
@@ -61,8 +61,8 @@ export default class News extends Component {
     // }
 
     fetchMoreData = async () => {  
-        this.setState({page: this.state.page + 1})
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+        this.setState({page: this.state.page + 1})
         let data = await fetch(url);
         let parsedData = await data.json()
         this.setState({
